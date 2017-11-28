a1906ebcda95432c5af9309383a181c7f3609b78
Peh Hui Shi <pehhuishi06@hotmail.com>
2017-11-28 14:09:01 +0800

    Able to display in details of the each characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,79 @@
-// const ul = document.getElementById('characters');
-//
-// function createNode(element) {
-//      return document.createElement(element);
-//  }
-//
-//  function append(parent, el) {
-//    return parent.appendChild(el);
-//  }
-//
-// fetch('https://swapi.co/api/people/') // Call the fetch function passing the url of the API as a parameter
-// .then((resp) => resp.json()) // Your code for handling the data you get from the API
-// .then(function(data) { // Here you get the data to modify as you please
-//     // Create and append the li's to the ul
-//     let characters = data.results;
-//     console.log(characters);
-//     return characters.map(function(character) {
-//       let li = createNode('li'),
-//           span = createNode('span');
-//
-//       span.innerHTML = `${character.name}`;
-//
-//       append(li, span);
-//       append(ul, li);
-//     })
-//     })
-// .catch(function(error) {
-//     console.log(error);
-//   })
-var characters = new Vue({
-  el : '#app',
-  data : {
-    status: 'Hello I using vue.js'
-  },
-  created : function (){
-    this.loadCharacters();
-  },
-  methods : {
-    loadCharacters : function() {
-      this.status = 'Loading...'//can add a loading animation if i want
-      var characters = this;
-      axios.get('https://swapi.co/api/people/')
-      .then(function(response){
-
-        let charactersArray = response.data.results;
-        var randCharacter = charactersArray[Math.floor(Math.random()* charactersArray.length)]
-        characters.status = randCharacter;
-      })
-      .catch(function(error) {
-        characters.status = "An error has occured" + error;
-      });
+axios.get("https://swapi.co/api/films/7/")
+  .then(response => {
+    characters = response.data.characters
+    randCharacter = characters[Math.floor(Math.random() * characters.length)]
+    // console.log(randCharacter);
+  })
+  .then(response => {
+    var vue = new Vue({
+      el: '#app',
+      created: function (){
+        this.loadCharacter();
+      },
+      methods: {
+        loadCharacter: function (){
+          this.status = 'Page Loading...';
+          axios.get(`${randCharacter}`)
+          .then(response => {
+            this.people = response.data
+            console.log(people);
+          })
+          .catch(error => {
+            this.status = 'An error occurred. ' + error;
+          });
+        }
+      },
+      data: {
+        random: randCharacter,
+        people: this.methods,
+        status: '',
+        images: [
+        { text: 'https://swapi.co/api/people/1/',
+          image: '/assets/images/luke.jpg',
+          background: '/assets/images/wallpaper2.jpg'
+        },
+        { text: 'https://swapi.co/api/people/3/',
+          image: '/assets/images/R2-D2.jpg',
+          background: '/assets/images/wallpaper2.jpg'
+        },
+        { text: 'https://swapi.co/api/people/5/',
+          image: '/assets/images/leya.jpg',
+          background: '/assets/images/wallpaper2s.jpg'
+        },
+        { text: 'https://swapi.co/api/people/13/',
+          image: '/assets/images/chew.jpg',
+          background: '/assets/images/wallpaper2.jpg'
+        },
+        { text: 'https://swapi.co/api/people/14/',
+          image: '/assets/images/han.jpg',
+          background: '/assets/images/wallpaper2.jpg'
+        },
+        { text: 'https://swapi.co/api/people/27/',
+          image: '/assets/images/ackbar.jpg',
+          background: '/assets/images/wallpaper2.jpg'
+        },
+        { text: 'https://swapi.co/api/people/84/',
+          image: '/assets/images/finn.jpg',
+          background: '/assets/images/wallpaper2.jpg'
+        },
+        { text: 'https://swapi.co/api/people/85/',
+          image: '/assets/images/rey.jpg',
+          background: '/assets/images/wallpaper2.jpg'
+        },
+        { text: 'https://swapi.co/api/people/86/',
+          image: '/assets/images/poe.jpg',
+          background: '/assets/images/wallpaper2.jpg'
+        },
+        { text: 'https://swapi.co/api/people/87/',
+          image: '/assets/images/bb8.jpg',
+          background: '/assets/images/wallpaper2.jpg'
+        },
+        { text: 'https://swapi.co/api/people/88/',
+          image: '/assets/images/phasma.jpg',
+          background: '/assets/images/wallpaper2.jpg'
+        }
+      ]
     }
-  }
-});
+
+    })
+  })
