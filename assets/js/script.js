f3630385e1b1e48f100504857824e1b7a487fc2a
Peh Hui Shi <pehhuishi06@hotmail.com>
2017-11-29 01:22:06 +0800

    Trying to make it responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,75 +2,77 @@ axios.get("https://swapi.co/api/films/7/")
   .then(response => {
     characters = response.data.characters
     randCharacter = characters[Math.floor(Math.random() * characters.length)]
-    // console.log(randCharacter);
   })
   .then(response => {
     var vue = new Vue({
       el: '#app',
       created: function (){
-        this.loadCharacter();
+        this.loadRandCharacter();
+        // this.loadCharacters();
       },
       methods: {
-        loadCharacter: function (){
+        loadRandCharacter: function (){
           this.status = 'Page Loading...';
-          axios.get(`${randCharacter}`)
+          axios.get(randCharacter)
           .then(response => {
             this.people = response.data
-            console.log(people);
+            // console.log(people);
           })
           .catch(error => {
             this.status = 'An error occurred. ' + error;
           });
         }
+        // loadCharacters: function (){
+        //   this.status = 'Page Loading...';
+        //   axios.get()
+        //   .then(response => {
+        //     let data = response.data
+        //     peoples =data.results
+        //     console.log(peoples);
+        //   })
+        //   .catch(error => {
+        //     this.status = 'An error occurred. ' + error;
+        //   });
+        // }
       },
       data: {
+        // characters: this.loadCharacters,
         random: randCharacter,
-        people: this.methods,
+        people: this.loadRandCharacter,
         status: '',
         images: [
         { text: 'https://swapi.co/api/people/1/',
-          image: '/assets/images/luke.jpg',
-          background: '/assets/images/wallpaper2.jpg'
+          image: '/assets/images/luke.png',
         },
         { text: 'https://swapi.co/api/people/3/',
-          image: '/assets/images/R2-D2.jpg',
-          background: '/assets/images/wallpaper2.jpg'
+          image: '/assets/images/r2-d2.png',
         },
         { text: 'https://swapi.co/api/people/5/',
-          image: '/assets/images/leya.jpg',
-          background: '/assets/images/wallpaper2s.jpg'
+          image: '/assets/images/leia.png',
         },
         { text: 'https://swapi.co/api/people/13/',
-          image: '/assets/images/chew.jpg',
-          background: '/assets/images/wallpaper2.jpg'
+          image: '/assets/images/Chewbacca.png',
         },
         { text: 'https://swapi.co/api/people/14/',
-          image: '/assets/images/han.jpg',
-          background: '/assets/images/wallpaper2.jpg'
+          image: '/assets/images/HanSolo.png',
         },
         { text: 'https://swapi.co/api/people/27/',
-          image: '/assets/images/ackbar.jpg',
-          background: '/assets/images/wallpaper2.jpg'
+          image: '/assets/images/Ackbar.png',
         },
         { text: 'https://swapi.co/api/people/84/',
-          image: '/assets/images/finn.jpg',
-          background: '/assets/images/wallpaper2.jpg'
+          image: '/assets/images/finn.png',
         },
         { text: 'https://swapi.co/api/people/85/',
-          image: '/assets/images/rey.jpg',
-          background: '/assets/images/wallpaper2.jpg'
+          image: '/assets/images/rey.png',
         },
         { text: 'https://swapi.co/api/people/86/',
-          image: '/assets/images/poe.jpg',
-          background: '/assets/images/wallpaper2.jpg'
+          image: '/assets/images/poe-dameron.png',
         },
         { text: 'https://swapi.co/api/people/87/',
-          image: '/assets/images/bb8.jpg',
-          background: '/assets/images/wallpaper2.jpg'
+          image: '/assets/images/bb8.png',
         },
         { text: 'https://swapi.co/api/people/88/',
-          image: '/assets/images/phasma.jpg',
-          background: '/assets/images/wallpaper2.jpg'
+          image: '/assets/images/captain-phasma.png',
         }
       ]
     }
