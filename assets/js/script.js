0fef766513f726c54b7e2ab98eafb2e613375789
Peh Hui Shi <pehhuishi06@hotmail.com>
2017-11-29 14:14:21 +0800

    make changes to the images link

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,11 @@ axios.get("https://swapi.co/api/films/7/")
       },
       methods: {
         loadRandCharacter: function (){
+          var vue = this;
           this.status = 'Page Loading...';
           axios.get(randCharacter)
           .then(response => {
-            this.people = response.data
+            vue.people = response.data
             // console.log(people);
           })
           .catch(error => {
@@ -42,37 +43,37 @@ axios.get("https://swapi.co/api/films/7/")
         status: '',
         images: [
         { text: 'https://swapi.co/api/people/1/',
-          image: '/assets/images/luke.png',
+          image: './assets/images/luke.png'
         },
         { text: 'https://swapi.co/api/people/3/',
-          image: '/assets/images/r2-d2.png',
+          image: './assets/images/r2-d2.png'
         },
         { text: 'https://swapi.co/api/people/5/',
-          image: '/assets/images/leia.png',
+          image: './assets/images/leia.png'
         },
         { text: 'https://swapi.co/api/people/13/',
-          image: '/assets/images/Chewbacca.png',
+          image: './assets/images/Chewbacca.png'
         },
         { text: 'https://swapi.co/api/people/14/',
-          image: '/assets/images/HanSolo.png',
+          image: './assets/images/HanSolo.png'
         },
         { text: 'https://swapi.co/api/people/27/',
-          image: '/assets/images/Ackbar.png',
+          image: './assets/images/Ackbar.png'
         },
         { text: 'https://swapi.co/api/people/84/',
-          image: '/assets/images/finn.png',
+          image: './assets/images/finn.png'
         },
         { text: 'https://swapi.co/api/people/85/',
-          image: '/assets/images/rey.png',
+          image: './assets/images/rey.png'
         },
         { text: 'https://swapi.co/api/people/86/',
-          image: '/assets/images/poe-dameron.png',
+          image: './assets/images/poe-dameron.png'
         },
         { text: 'https://swapi.co/api/people/87/',
-          image: '/assets/images/bb8.png',
+          image: './assets/images/bb8.png'
         },
         { text: 'https://swapi.co/api/people/88/',
-          image: '/assets/images/captain-phasma.png',
+          image: './assets/images/captain-phasma.png'
         }
       ]
     }
