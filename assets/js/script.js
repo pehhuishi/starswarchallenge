9f97212d2e84967e8ffe1e6a69fb3d9d452e3734
Peh Hui Shi <pehhuishi06@hotmail.com>
2017-11-29 23:37:15 +0800

    arrange the js file properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,22 @@ axios.get("https://swapi.co/api/films/7/")
   .then(response => {
     var vue = new Vue({
       el: '#app',
-      created: function (){
+      created: function() {
         this.loadRandCharacter();
         // this.loadCharacters();
       },
       methods: {
-        loadRandCharacter: function (){
+        loadRandCharacter: function() {
           var vue = this;
           this.status = 'Page Loading...';
           axios.get(randCharacter)
-          .then(response => {
-            vue.people = response.data
-            // console.log(people);
-          })
-          .catch(error => {
-            this.status = 'An error occurred. ' + error;
-          });
+            .then(response => {
+              vue.people = response.data
+              // console.log(people);
+            })
+            .catch(error => {
+              this.status = 'An error occurred. ' + error;
+            });
         }
         // loadCharacters: function (){
         //   this.status = 'Page Loading...';
@@ -41,42 +41,52 @@ axios.get("https://swapi.co/api/films/7/")
         random: randCharacter,
         people: this.loadRandCharacter,
         status: '',
-        images: [
-        { text: 'https://swapi.co/api/people/1/',
-          image: './assets/images/luke.png'
-        },
-        { text: 'https://swapi.co/api/people/3/',
-          image: './assets/images/r2-d2.png'
-        },
-        { text: 'https://swapi.co/api/people/5/',
-          image: './assets/images/leia.png'
-        },
-        { text: 'https://swapi.co/api/people/13/',
-          image: './assets/images/Chewbacca.png'
-        },
-        { text: 'https://swapi.co/api/people/14/',
-          image: './assets/images/HanSolo.png'
-        },
-        { text: 'https://swapi.co/api/people/27/',
-          image: './assets/images/Ackbar.png'
-        },
-        { text: 'https://swapi.co/api/people/84/',
-          image: './assets/images/finn.png'
-        },
-        { text: 'https://swapi.co/api/people/85/',
-          image: './assets/images/rey.png'
-        },
-        { text: 'https://swapi.co/api/people/86/',
-          image: './assets/images/poe-dameron.png'
-        },
-        { text: 'https://swapi.co/api/people/87/',
-          image: './assets/images/bb8.png'
-        },
-        { text: 'https://swapi.co/api/people/88/',
-          image: './assets/images/captain-phasma.png'
-        }
-      ]
-    }
+        images: [{
+            text: 'https://swapi.co/api/people/1/',
+            image: './assets/images/luke.png'
+          },
+          {
+            text: 'https://swapi.co/api/people/3/',
+            image: './assets/images/r2-d2.png'
+          },
+          {
+            text: 'https://swapi.co/api/people/5/',
+            image: './assets/images/leia.png'
+          },
+          {
+            text: 'https://swapi.co/api/people/13/',
+            image: './assets/images/Chewbacca.png'
+          },
+          {
+            text: 'https://swapi.co/api/people/14/',
+            image: './assets/images/HanSolo.png'
+          },
+          {
+            text: 'https://swapi.co/api/people/27/',
+            image: './assets/images/Ackbar.png'
+          },
+          {
+            text: 'https://swapi.co/api/people/84/',
+            image: './assets/images/finn.png'
+          },
+          {
+            text: 'https://swapi.co/api/people/85/',
+            image: './assets/images/rey.png'
+          },
+          {
+            text: 'https://swapi.co/api/people/86/',
+            image: './assets/images/poe-dameron.png'
+          },
+          {
+            text: 'https://swapi.co/api/people/87/',
+            image: './assets/images/bb8.png'
+          },
+          {
+            text: 'https://swapi.co/api/people/88/',
+            image: './assets/images/captain-phasma.png'
+          }
+        ]
+      }
 
     })
   })
